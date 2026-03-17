def18dc40b8ae293ae1a7d278c8918cfc6b0c0fb
伊藤 洋也 <hito@pepabo.com>
2026-03-17 14:32:17 +0900

    feat: update mde-cli to v0.1.1
    microsoft-defender-for-endpoint-cli の v0.1.1 リリースに対応して
    version と sha256 チェックサムを更新します。

--- Formula/mde-cli.rb
+++ Formula/mde-cli.rb
@@ -1,30 +1,30 @@
 class MdeCli < Formula
   desc "A command-line tool for Microsoft Defender for Endpoint, written in Rust"
   homepage "https://github.com/hiboma/microsoft-defender-for-endpoint-cli"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/hiboma/microsoft-defender-for-endpoint-cli/releases/download/v#{version}/mde-cli-aarch64-apple-darwin.tar.gz"
-      sha256 "4fdf5024657cf95ee1ef57106731ec64f28c4655dfca790ebb71036945d40a0d"
+      sha256 "69721c1c037c13f2254eda6810668e7f429b2d5a71be34e06e04d2147c945bcb"
     end
 
     on_intel do
       url "https://github.com/hiboma/microsoft-defender-for-endpoint-cli/releases/download/v#{version}/mde-cli-x86_64-apple-darwin.tar.gz"
-      sha256 "e2a15a33428c6453901d0f85cb4456acf01b76b0ae3e967bea4f643a26e511ee"
+      sha256 "20d810ec5c7b9468d566c367c6bc37c0f6b0322e2650457e7b11f8a71d475170"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/hiboma/microsoft-defender-for-endpoint-cli/releases/download/v#{version}/mde-cli-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "5d6355f82ac56c7f8ac217cbe997be2478ae4d95b8db199e7fa0d77e9dba7531"
+      sha256 "631cdb242d80a78f755682c7a2e4fd70d4c82fe235627dfb7436c2ffb16b711b"
     end
 
     on_intel do
       url "https://github.com/hiboma/microsoft-defender-for-endpoint-cli/releases/download/v#{version}/mde-cli-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "46db33040bdb27a8545bcdd0c5574ea6be30dd92d11d2c72ac618fe22d85f553"
+      sha256 "c54e5748e15cf71f51ac32df91ae1b95eb57374508d91bb9af43f087b18103d2"
     end
   end
 
